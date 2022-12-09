06f0a3e674994bc1df7f63a75af8279ff2834623
Oleksandr Dudko <alexdoodko@gmail.com>
2022-12-09 15:26:59 +0200

    Add missed commands

--- handlers/commands.py
+++ handlers/commands.py
@@ -13,9 +13,44 @@ router.message.filter(F.from_user.id.in_(config.admins))
 @router.message(Command(commands=['send_message']))
 async def cmd_send_message(message: Message):
     msg = message.text[13:]
-    await bot.send_message(chat_id='-740613554', text=msg)
+    await bot.send_message(chat_id='-1001092707720', text=msg)
 
 
 @router.message(Command(commands=['health_check']))
 async def cmd_health_check(message: Message):
     await message.answer(text="I'm okay!")
+
+
+@router.message(Command(commands=['ask_volodya']))
+async def cmd_ask_volodya(message: Message):
+    await message.delete()
+
+    if message.reply_to_message:
+        query = 'одним словом'
+        command = message.text.split()[0]
+
+        if len(message.text.split()) > 1:
+            query = message.text.replace(command, "").strip()
+
+        answer = f"""
+        Спробуйте запитати у Володі:
+        1. Відкриваємо чат з ботом @pkvartal_bot
+        2. Пишемо йому запит <b>{query}</b>
+        3. Отримуємо релевантні результати. Профіт!"""
+
+        await message.reply_to_message.reply(text=answer)
+
+
+@router.message(Command(commands=['read_rules']))
+async def cmd_read_ruled(message: Message):
+    await message.delete()
+
+    if message.reply_to_message:
+        user = message.reply_to_message.from_user
+        await message.reply_to_message.delete()
+
+        rules_url = 'https://telegra.ph/Pravila-grupi-Petr%D1%96vskij-kvartal-10-20'
+        answer = f"[{user.full_name}](tg://user?id={user.id}), ознайомтесь з " \
+                 f"[правилами групи]({rules_url}), будь ласка\."
+
+        await message.answer(text=answer, parse_mode='MarkdownV2')
