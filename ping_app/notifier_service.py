5d791a34fce258aeb207611b0d6cbde65a1fb48b
Oleksandr Dudko <alexdoodko@gmail.com>
2022-12-18 10:25:52 +0200

    Minor changes in notifier

--- ping_app/notifier_service.py
+++ ping_app/notifier_service.py
@@ -10,18 +10,18 @@ class NotifierService:
 
     def get_current_state(self, instance: Zone | Host) -> str:
         status = instance.is_online
-        return f"{self._emodji[status]} {instance.name} {self._new_status[not status]} о " \
+        return f"{self._emodji[status]} <b>{instance.name}</b> {self._new_status[not status]} о " \
                f"{instance.updated_at.strftime('%H:%M')}, " \
                f"{self._current_status[status]} вже {self._calculate_hours_and_minutes(instance)}"
 
     def get_changed_state(self, instance: Zone | Host) -> str:
-        return f"{self._emodji[not instance.is_online]} {instance.name} {self._new_status[instance.is_online]}. " \
+        return f"{self._emodji[not instance.is_online]} <b>{instance.name}</b> {self._new_status[instance.is_online]}. " \
                f"Були {self._current_status[instance.is_online]} {self._calculate_hours_and_minutes(instance)}"
 
     @staticmethod
     def _calculate_hours_and_minutes(instance: Zone | Host) -> str:
         seconds_since_last_update = (datetime.now() - instance.updated_at).seconds
-        hours: int = seconds_since_last_update // 3600
+        hours: str = str(seconds_since_last_update // 3600)
         minutes: int = seconds_since_last_update % 3600 // 60
 
-        return f"{hours} год. {minutes:02} хв."
+        return f"{hours + ' год. ' if hours else ''}{minutes:02} хв."
