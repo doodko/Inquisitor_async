8fc26a3e69d804f320e38bb1196550916abf235c
Oleksandr Dudko <alexdoodko@gmail.com>
2022-12-24 08:19:39 +0200

    Add subscription notifications

--- start_ping.py
+++ start_ping.py
@@ -9,7 +9,6 @@ from ping_app.ping_service import PingService
 logger.remove()
 logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{message}</level>")
 logger.add("logs/{time:YYYY-MM-DD}_ping.log", format="{time:YYYY-MM-DD HH:mm:ss} | {message}", level='INFO')
-# logger.add('logs/info.log', format="{time:YYYY-MM-DD HH:mm:ss} | {message}", level='INFO')
 
 
 
