109582131f3ae4b418ed3dbeb40e5b6fd2b2fbf9
Oleksandr Dudko <alexdoodko@gmail.com>
2023-01-02 22:26:19 +0200

    Add ping_period command and controller with validation

--- bot/handlers/ping.py
+++ bot/handlers/ping.py
@@ -24,3 +24,14 @@ async def cmd_update_time(message: Message):
     command, zone_id, new_time = message.text.split()
     text = await host_crud_service.update_zone_time(zone_id=zone_id, new_time=new_time)
     await message.answer(text=text)
+
+
+@router.message(Command(commands=['ping_period']))
+async def cmd_ping_period(message: Message):
+    lst = message.text.split()
+    if len(lst) == 1:
+        await message.answer(f"ping period is {config.ping_period} seconds now")
+    elif len(lst) == 2:
+        command, new_period = lst
+        answer = await ps.change_ping_periodicity(new_period)
+        await message.answer(answer)
