a96931990075386f8ae42846aa9d4c49d76ceb9e
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-10 08:38:47 +0300

    update private messages handler to process search

--- bot/handlers/private_messages.py
+++ bot/handlers/private_messages.py
@@ -3,6 +3,8 @@ from aiogram.types import Message
 from loguru import logger
 
 from bot.handlers.commands import cmd_donate
+from bot.services.search_service import search_service
+from bot.types.search_dto import SearchResponse
 
 router = Router()
 router.message.filter(F.chat.type =='private')
@@ -15,11 +17,24 @@ async def text_donate(message: Message):
 
 @router.message()
 async def all_other_private_messages(message: Message):
-    text = f"other messages | {message.from_user.full_name} | {message.text}"
-    logger.bind(private=True).info(text)
-    text = '''
-На даний момент бот знаходиться в стадії бета тестування. Якщо помітили помилку - напишіть @doodko
-'''
+    log_text = f"other messages | {message.from_user.full_name} | {message.text}"
+    logger.bind(private=True).info(log_text)
 
-    await message.answer(text=text)
+    if len(message.text) < 3:
+        answer_text = "Your search query should contain at least 3 symbols."
+    elif len(message.text.split()) > 3:
+        answer_text = "Your search query should contain no more than 3 words."
+    else:
+        search_response = search_service.find(message.text)
+
+        if not search_response:
+            answer_text = "Сталась якась халепа, спробуйте пізніше :("
+
+        elif search_response.count == 0:
+            answer_text = "Мені не вдалось нічого знайти :("
+
+        else:
+            answer_text = f"Знайдено {search_response.count} результатів"
+
+    await message.answer(text=answer_text)
 
