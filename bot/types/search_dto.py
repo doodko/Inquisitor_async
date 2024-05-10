ef40894140615a0eda3f01b3808692082d57b270
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-10 08:37:48 +0300

    add establishment dto

---
+++ bot/types/search_dto.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, HttpUrl
+
+
+class SocialContact(BaseModel):
+    name: str
+    value: HttpUrl
+
+class Establishment(BaseModel):
+    id: int
+    name: str
+    slug: str
+    altname: str | None = None
+    description: str | None = None
+    address: str | None = None
+    hint: str | None = None
+    tags: List[str]
+    category: str
+    location: str | None = None
+    workhrs: str | None = None
+    phone_numbers: List[str]
+    social_contact: List[SocialContact]
+    logo: Optional[HttpUrl]
+    created_at: datetime
+    updated_at: datetime
+
+class SearchResponse(BaseModel):
+    count: int
+    pages: int
+    result: List[Establishment]
