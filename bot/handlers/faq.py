608e1172966d63d9beda7c84a0729b107bca1d11
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-10 07:18:39 +0300

    clean up files

--- bot/handlers/faq.py
+++ bot/handlers/faq.py
@@ -1,11 +1,8 @@
 from aiogram import Router, F
-from aiogram.exceptions import TelegramForbiddenError
 from aiogram.types import Message
 
-from ping_app.ping_service import PingService
 
 router = Router()
-ps = PingService()
 
 
 regexp_base = r".*((дайте)|.*(ка[жз])|(пиш)|(какой)|(який)).*"
@@ -13,8 +10,6 @@ ohorona = regexp_base + r"((телефон)|(номер)).*(ох[о]?р[оа]н[
 service_company = regexp_base + r"((телефон)|(номер)).*((ж[єкеэ][хк])|(комфорт.серв[иі]с))"
 post_index = regexp_base + r"([іи]ндекс)"
 
-kivi = r".*\b(кав[ауио]|кофе|ф[иі]льтр|[эе]спре[с]+о|капуч[іи]но|латте|п[иі][ц]+а|сендв[иі]ч|бургер|сніданок|завтрак|" \
-       r"тістечка|круасан|смаколик|торт|макар[оу]н|кафе|ресторан|кав\'ярн|п[иі][ц]+ер[иі])"
 
 
 @router.message(F.text.lower().regexp(ohorona))
@@ -30,11 +25,3 @@ async def say_service_company_phone(message: Message):
 @router.message(F.text.lower().regexp(post_index))
 async def say_index(message: Message):
     await message.reply("індекс: 08148")
-
-
-@router.message(F.text.lower().regexp(kivi))
-async def ping_kivi(message: Message):
-    try:
-        await message.forward(chat_id=798798016)
-    except TelegramForbiddenError:
-        pass
