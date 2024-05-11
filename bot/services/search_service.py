c23d1794d663a064a4804f8b41a1746a1a16690a
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-11 09:03:04 +0300

    refactoring search service

--- bot/services/search_service.py
+++ bot/services/search_service.py
@@ -1,6 +1,6 @@
 import requests
 
-from bot.types.search_dto import SearchResponse
+from bot.types.search_dto import SearchResponse, Establishment
 from settings_reader import config
 
 
@@ -8,23 +8,30 @@ class SearchService:
     def __init__(self):
         self.api_url = config.search_api
 
-    def _search(self, query: str):
-        response = requests.get(self.api_url, params={"search": query, "page_size": 20})
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f"Request failed with status code: {response.status_code}")
-            return None
-
-    def find(self, query: str) -> SearchResponse | None:
+    def _request(self, endpoint, params=None):
         try:
-            response_data = self._search(query)
-            search_response = SearchResponse.model_validate(response_data)
-            return search_response
-
+            response = requests.get(endpoint, params=params)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print(f"Request failed with status code: {response.status_code}")
         except Exception as e:
             print(f"Error occurred: {e}")
             return None
+
+    def find(self, query: str) -> SearchResponse:
+        endpoint = self.api_url
+        params = {"search": query, "page_size": 20}
+
+        response_data = self._request(endpoint, params)
+        search_response = SearchResponse.model_validate(response_data)
+        return search_response
+
+    def retrieve(self, slug: str) -> Establishment:
+        endpoint = self.api_url + slug
+        response_data = self._request(endpoint)
+        establishment = Establishment.model_validate(response_data)
+        return establishment
 
 
 search_service = SearchService()
