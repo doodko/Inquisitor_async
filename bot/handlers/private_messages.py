8d6e014d03101f7c5a58082c059c3235ddbd322d
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-11 07:27:46 +0300

    remove unused imports

--- bot/handlers/private_messages.py
+++ bot/handlers/private_messages.py
@@ -1,10 +1,8 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from loguru import logger
 
 from bot.handlers.commands import cmd_donate
 from bot.services.private_message_service import private_message_service
-from bot.services.search_service import search_service
 
 router = Router()
 router.message.filter(F.chat.type =='private')
