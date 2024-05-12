24fcdbee9466adcce991a4f91f433e8db516570a
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-12 22:34:55 +0300

    fix donate button

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -62,7 +62,7 @@ async def cmd_donate(message: Message):
         builder.row(InlineKeyboardButton(text="Підтримати", url=config.jar_url))
 
         answer = "Подобається сервіс? Ви можете подякувати та підтримати розробника монетою 💰"
-        await message.answer(text=answer, parse_mode="MarkdownV2")
+        await message.answer(text=answer, reply_markup=builder.as_markup())
 
     else:
         await message.delete()
@@ -71,7 +71,7 @@ async def cmd_donate(message: Message):
 @router.message(Command(commands=["start", "help"]))
 async def cmd_help(message: Message):
     if message.chat.type == "private":
-        answer = f"Коротко запитайте що вас цікавить і я спробую знайти варіанти серед закладів ПК. Я вмію шукати по назві чи ключовим словам.\n\nЗнайшли помилку? <a href='tg://user?id={config.superuser_id}'>Пишіть.</a>"
+        answer = f"Коротко запитайте що вас цікавить і я спробую знайти варіанти серед закладів ПК. Я вмію шукати по назві чи ключовим словам.\n\nЗнайшли помилку чи хочете запропонувати зміни? <a href='tg://user?id={config.superuser_id}'>Пишіть.</a>"
         await message.answer(text=answer)
     else:
         await message.delete()
