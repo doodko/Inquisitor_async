281d03eaefc2832f730ea6aa78cbf61b0e18ea31
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-14 07:16:50 +0300

    extend ApiClient, add user in headers and post requests

--- bot/services/api_client.py
+++ bot/services/api_client.py
@@ -1,16 +1,25 @@
 import requests
+from aiogram.types import User
 
 from bot.settings_reader import config
 from bot.types.search_dto import Establishment, SearchResponse
 
 
 class ApiClient:
-    def __init__(self):
+    def __init__(self, user: User):
         self.api_url = config.api_url
+        self.user = user
 
-    def _request(self, endpoint, params=None):
+    def _request(self, endpoint: str, method: str = "GET", params=None, data=None):
+        headers = {"telegram_user_id": str(self.user.id)}
         try:
-            response = requests.get(endpoint, params=params)
+            if method == "GET":
+                response = requests.get(endpoint, params=params, headers=headers)
+            elif method == "POST":
+                response = requests.post(endpoint, json=data, headers=headers)
+            else:
+                raise ValueError("Unsupported HTTP method")
+
             if response.status_code == 200:
                 return response.json()
             else:
@@ -23,15 +32,27 @@ class ApiClient:
         endpoint = self.api_url
         params = {"search": query, "page_size": 20}
 
-        response_data = self._request(endpoint, params)
+        response_data = self._request(endpoint=endpoint, params=params)
         search_response = SearchResponse.model_validate(response_data)
         return search_response
 
     def retrieve(self, slug: str) -> Establishment:
         endpoint = self.api_url + slug + "/"
-        response_data = self._request(endpoint)
+        response_data = self._request(endpoint=endpoint)
         establishment = Establishment.model_validate(response_data)
         return establishment
 
+    def hello_its_me(self, user: User):
+        endpoint = self.api_url
+        data = {
+            "tg_id": user.id,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "username": user.username,
+            "is_premium": user.is_premium,
+        }
+        response_data = self._request(endpoint=endpoint, method="POST", data=data)
+        print(response_data)
 
-api_client = ApiClient()
+    def vote(self, establishment_id: int, vote: int):
+        print(f"Voting for {establishment_id}, user {self.user.id} rated it {vote}")
