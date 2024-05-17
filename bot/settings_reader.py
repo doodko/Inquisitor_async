a6e815914c8486047d04d0c24e3b60c00a9173f9
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-17 07:51:10 +0300

    integrate sentry monitoring

--- bot/settings_reader.py
+++ bot/settings_reader.py
@@ -5,11 +5,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "Inquisitor"
     token: SecretStr
+    app_env: str = "development"
     superuser_id: int
     admins: set
     api_url: str
     rules_url: str
     jar_url: str
+    sentry_dns: str
 
     class Config:
         env_file = ".env"
