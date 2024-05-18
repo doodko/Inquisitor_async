107e22452acba982beb29fe145b51840b3378b8d
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-18 18:30:02 +0300

    track events in mixpanel

--- bot/services/private_message_service.py
+++ bot/services/private_message_service.py
@@ -4,7 +4,8 @@ from aiogram import types
 from loguru import logger
 
 from bot.services.api_client import ApiClient
-from bot.types.enums import AnswerTypes
+from bot.services.mixpanel_client import mp
+from bot.types.enums import AnswerTypes, MixpanelEvents
 from bot.types.message_answers import MessageAnswers
 from bot.types.search_dto import Establishment, SearchResponse
 
@@ -15,34 +16,70 @@ class PrivateMessageService:
     ) -> str | List[Establishment]:
         await self.log_message(message=message)
 
-        if validation_error := await self.validate_message(query=message.text):
+        if validation_error := await self.validate_message(message=message):
             return validation_error
 
         return await self.perform_search(message=message)
 
     @staticmethod
     async def log_message(message: types.Message):
-        log_text = f"other messages | {message.from_user.full_name} | {message.text}"
+        log_text = f"search message | {message.from_user.full_name} | {message.text}"
         logger.bind(private=True).info(log_text)
 
     @staticmethod
-    async def validate_message(query: str) -> str | None:
+    async def validate_message(message: types.Message) -> str | None:
+        query = message.text
         if len(query) < 3:
-            return MessageAnswers.answer(AnswerTypes.TOO_SHORT_TEXT)
+            answer = MessageAnswers.answer(AnswerTypes.TOO_SHORT_TEXT)
+            mp.track_event(
+                user_id=message.from_user.id,
+                event=MixpanelEvents.TOO_SHORT_MESSAGE,
+                event_properties={"type": "search", "message": query, "answer": answer},
+            )
+            return answer
+
         elif len(query.split()) > 3:
-            return MessageAnswers.answer(AnswerTypes.TOO_MANY_WORDS)
+            answer = MessageAnswers.answer(AnswerTypes.TOO_MANY_WORDS)
+            mp.track_event(
+                user_id=message.from_user.id,
+                event=MixpanelEvents.TOO_LONG_MESSAGE,
+                event_properties={"type": "search", "message": query, "answer": answer},
+            )
+            return answer
 
     @staticmethod
     async def perform_search(message: types.Message) -> str | SearchResponse:
         query = message.text
         api_client = ApiClient(user=message.from_user)
         search_response = api_client.find(query=query)
+
         if not search_response:
             answer = MessageAnswers.answer(AnswerTypes.ERROR_MESSAGE)
+            mp.track_event(
+                user_id=message.from_user.id,
+                event=MixpanelEvents.ERROR,
+                event_properties={"type": "search", "message": query, "answer": answer},
+            )
+
         elif search_response.count == 0:
             answer = MessageAnswers.answer(AnswerTypes.NO_RESULTS_FOUND)
+            mp.track_event(
+                user_id=message.from_user.id,
+                event=MixpanelEvents.UNSUCCESSFUL_SEARCH,
+                event_properties={"type": "search", "message": query, "answer": answer},
+            )
+
         else:
             answer = search_response
+            mp.track_event(
+                user_id=message.from_user.id,
+                event=MixpanelEvents.SEARCH,
+                event_properties={
+                    "type": "search",
+                    "message": query,
+                    "answer": search_response.count,
+                },
+            )
 
         return answer
 
