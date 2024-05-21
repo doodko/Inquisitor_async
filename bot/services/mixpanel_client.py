7eac89636fb55896d3241a3b43c397a0a33b7b0b
Oleksandr Dudko <alexdoodko@gmail.com>
2024-05-21 08:20:38 +0300

    pass user instead of user_id in mixpanel client, add logging

--- bot/services/mixpanel_client.py
+++ bot/services/mixpanel_client.py
@@ -1,5 +1,6 @@
 import mixpanel
 from aiogram.types import User
+from loguru import logger
 
 from bot.settings_reader import config
 from bot.types.enums import MixpanelEvents
@@ -13,9 +14,14 @@ class MixpanelClient:
         )
 
     def track_event(
-        self, user_id: int, event: MixpanelEvents, event_properties: dict = None
+        self, user: User, event: MixpanelEvents, event_properties: dict = None
     ):
-        self.mixpanel_instance.track(str(user_id), event, event_properties)
+        self.mixpanel_instance.track(str(user.id), event, event_properties)
+        event_log = " | ".join(
+            [f"{key}: {str(value)}" for key, value in event_properties.items()]
+        )
+        log_text = f"{event} | {user.full_name} | {event_log}"
+        logger.bind(event=True).info(log_text)
 
     def update_user_properties(self, user: User):
         properties = {
